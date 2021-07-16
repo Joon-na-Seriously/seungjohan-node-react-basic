7e79c1f86eea3ee8b14cc1f3f5254e655df4eb90
seungjohan <cmostrillion@gmail.com>
2021-07-16 14:42:15 +0900

    setting to push without node_modules

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://seungjo:likelion1234@boilerplate.copia.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  userNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+    }
+).then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err))
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -9,3 +17,4 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
---
+++ models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+//Schema
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        trim: true, //user가 실수로 스페이스바를 쳤을 때 이를 없애주는 것.
+        // ex) tmd wh7275@naver.com -> tmdwh7275@naver.com
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxLength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    }, // 어떤 user가 관리자가 될 수도 일반user가 될 수도 있기에 이를 관리하고자
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    } // token 사용 유효기간
+})
+
+//Schema를 Model로 감싸준다
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
